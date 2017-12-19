a51d30199c87f650d09d71eae599e4ac4a203ce0
ungacy <ungacy@126.com>
2017-12-19 15:59:36 +0800

    init

---
+++ Example/Pods/Target Support Files/Pods-SIUIKit_Example/Pods-SIUIKit_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SIUIKit_Example : NSObject
+@end
+@implementation PodsDummy_Pods_SIUIKit_Example
+@end
---
+++ Example/Pods/Target Support Files/SIUIKit/SIUIKit-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_SIUIKit : NSObject
+@end
+@implementation PodsDummy_SIUIKit
+@end
