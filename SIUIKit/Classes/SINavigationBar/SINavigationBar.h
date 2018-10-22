d13bc84752d04b195ce5143a049459b13ca77e0b
ungacy <ungacy@126.com>
2018-10-22 17:45:15 +0800

    version bump to 0.1.44

--- SIUIKit/Classes/SINavigationBar/SINavigationBar.h
+++ SIUIKit/Classes/SINavigationBar/SINavigationBar.h
@@ -132,4 +132,9 @@
                   resource:(NSString *)resource
                   selector:(SEL)selector;
 
+/*!
+ *  @brief 导航栏上元素离top的距离.默认20
+ */
+@property (nonatomic, assign) CGFloat topBaseline;//default is status bar height
+
 @end
